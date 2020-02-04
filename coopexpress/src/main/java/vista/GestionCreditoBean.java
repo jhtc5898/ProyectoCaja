d3bf3a3ab088c2e8a70b2b4ef7e424167fbade25
John Henry <John Henry@LAPTOP-CVDM5H62>
2020-02-04 16:00:53 -0500

    Pruebas De Pdf Dentro Del Server

--- coopexpress/src/main/java/vista/GestionCreditoBean.java
+++ coopexpress/src/main/java/vista/GestionCreditoBean.java
@@ -1,5 +1,9 @@
 package vista;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +11,14 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import modelo.Credito;
 import modelo.Credito_Detalle;
@@ -39,6 +51,7 @@ public class GestionCreditoBean {
 	
 	public String guardarSolicitudCredito() {
 		gc.guardar(credito);
+		writePDF();
 		init();
 		return "informe-credito";
 	}
@@ -54,6 +67,87 @@ public class GestionCreditoBean {
 		guardarSolicitudCredito();
 		return null;
 	}
+	
+	
+	 private void writePDF() {
+		 
+	        Document document = new Document();
+	 
+	        try {
+	        	String path = new File(".").getCanonicalPath();
+	        	String FILE_NAME = path + "/"+credito.getDesripcion_credito()+".pdf";
+	        	System.out.println(FILE_NAME);
+	        	
+	            PdfWriter.getInstance(document, new FileOutputStream(new File(FILE_NAME)));
+	 
+	            document.open();
+	 
+	            Paragraph paragraphHello = new Paragraph();
+	            paragraphHello.add("Solicitudo De Credto");
+	            paragraphHello.setAlignment(Element.ALIGN_JUSTIFIED);
+	 
+	            document.add(paragraphHello);
+	 
+	            Paragraph paragraphLorem = new Paragraph();
+	            paragraphLorem.add("Esta Es Una Prueba De Sus Accion Dentro De La Coperativa CoopExpress: Usted Solicito Un Monto : "
+	            		+ credito.getMonto_credito()
+	            		+ " Con Una Cantidad De Cuotas : " + credito.getNumero_meses_credito()
+	            		+ " En La Fecha " + credito.getFecha_solicitud_credito()
+	            		+ " Esta Solicitud Se Encuentra En Estado : " + credito.getEstado_credito()
+	            		);
+	            
+	            java.util.List<Element> paragraphList = new ArrayList<>();
+	            
+	            paragraphList = paragraphLorem.breakUp();
+	 
+	            Font f = new Font();
+	            f.setFamily(FontFamily.COURIER.name());
+	            f.setStyle(Font.BOLDITALIC);
+	            f.setSize(8);
+	            
+	            Paragraph p3 = new Paragraph();
+	            p3.setFont(f);
+	            p3.addAll(paragraphList);
+	            p3.add("Archivo Den Gran Importacia");
+	 
+	            //document.add(paragraphLorem);
+	            document.add(p3);
+	            document.close();
+	 
+	        } catch (FileNotFoundException | DocumentException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+				e.printStackTrace();
+			}
+	 
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public void getPagosCredito(String numeroCuenta) {
 		creditoDetalles = gcd.getPagos(Integer.parseInt(numeroCuenta));
